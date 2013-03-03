dcf9bae5e1ccfa49499510e8a83f3d12271eed7f
Sean Kimable <seankimable@gmail.com>
2013-03-03 11:18:44 -0800

    Initial Commit

---
+++ website/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  website
+//
+//  Created by Anonymous on 3/3/13.
+//  Copyright (c) 2013 Anonymous. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
